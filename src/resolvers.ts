cbcabf96ab4eb2d24b0867512d242fc57c2de2d8
Joe Bello <joebello123@proton.me>
2022-05-23 20:43:13 -0400

    dev: mocked responses

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -8,6 +8,20 @@ const resolvers = {
 				createdAt: 'string',
 				lastModified: 'string'
 			}
+		},
+		task() {
+			return {
+				id: 2,
+				author: {
+					id: 1,
+					username: 'derp',
+					createdAt: 'string',
+					lastModified: 'string'
+				},
+				createdAt: 'string',
+				modifiedAt: 'string',
+				title: 'Clean the dishes'
+			}
 		}
 	}
 }
