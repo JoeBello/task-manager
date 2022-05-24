7d09f0094721a565af2f57630d7eae7b615c270d
Joe Bello <joebello123@proton.me>
2022-05-23 20:42:45 -0400

    dev-feat: type definitions

--- src/schema.ts
+++ src/schema.ts
@@ -2,14 +2,52 @@ import gql from 'graphql-tag'
 
 const typeDefs = gql`
 	type User {
-		id: ID!
-		username: String!
 		createdAt: String!
-		lastModified: String!
+		id: ID!
+		modifiedAt: String!
+		tasks: [Task]!
+		username: String!
+	}
+
+	input NewUserInput {
+		username: String!
+	}
+
+	input UpdateUserInput {
+		id: ID!
+		username: String
+	}
+
+	type Task {
+		author: User!
+		createdAt: String!
+		description: String
+		id: ID!
+		modifiedAt: String!
+		title: String!
+	}
+
+	input NewTaskInput {
+		description: String
+		title: String!
+	}
+
+	input UpdateTaskInput {
+		description: String
+		id: ID!
+		title: String
 	}
 
 	type Query {
 		user: User!
+		task: Task!
+	}
+
+	type Mutation {
+		newTask(input: NewTaskInput!): User!
+		newUser(input: NewUserInput!): User!
+		updateTask(input: UpdateTaskInput!): User!
+		updateUser(input: UpdateUserInput!): User!
 	}
 `
 
