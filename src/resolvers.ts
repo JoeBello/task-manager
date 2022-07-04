e60a7e1cac30abea7c37de2c2ae5a24f0920a3d3
Joe Bello <joebello123@proton.me>
2022-07-04 17:35:18 -0400

    dev: models

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,44 +1,47 @@
-import { User, Task } from './model'
+import { Context, Task, User } from './Models'
 
 const resolvers = {
 	Query: {
-		user(parent: Record<string, any>, args: Record<string, any>, context: Record<string, any>) {
-			return User.getById(args.id, context)
-		},
-		task(parent: Record<string, any>, args: Record<string, any>, context: Record<string, any>) {
-			return Task.getById(args.id, context)
+		user(parent: Record<string, any>, args: Record<string, any>, context: Context) {
+			return User.findById(context, args.id)
 		}
+		// ,
+		// task(parent: Record<string, any>, args: Record<string, any>, context: Record<string, any>) {
+		// 	return Task.getById(args.id, context)
+		// }
 	},
 	Mutation: {
-		newUser(parent: Record<string, any>, args: Record<string, any>) {
-			return User.create(args.input)
+		newUser(parent: Record<string, any>, args: Record<string, any>, context: Context) {
+			return User.create(context, args.input)
 		},
-		updateUser(parent: Record<string, any>, args: Record<string, any>) {
-			return User.update(args.input)
+		updateUser(parent: Record<string, any>, args: Record<string, any>, context: Context) {
+			return User.update(context, args.input)
 		},
-		deleteUser(parent: Record<string, any>, args: Record<string, any>) {
-			return User.delete(args.id)
-		},
-		newTask(parent: Record<string, any>, args: Record<string, any>) {
-			return Task.create(args.input)
-		},
-		updateTask(parent: Record<string, any>, args: Record<string, any>) {
-			return Task.update(args.input)
-		},
-		deleteTask(parent: Record<string, any>, args: Record<string, any>) {
-			return Task.delete(args.id)
+		deleteUser(parent: Record<string, any>, args: Record<string, any>, context: Context) {
+			return User.delete(context, args.id)
 		}
-	},
-	User: {
-		tasks(parent: Record<string, any>) {
-			return Task.getByUserId(parent.id)
-		}
-	},
-	Task: {
-		user(parent: Record<string, any>) {
-			return User.getByTaskId(parent.id)
-		}
+		// ,
+		// newTask(parent: Record<string, any>, args: Record<string, any>) {
+		// 	return Task.create(args.input)
+		// },
+		// updateTask(parent: Record<string, any>, args: Record<string, any>) {
+		// 	return Task.update(args.input)
+		// },
+		// deleteTask(parent: Record<string, any>, args: Record<string, any>) {
+		// 	return Task.delete(args.id)
+		// }
 	}
+	// ,
+	// User: {
+	// 	tasks(parent: Record<string, any>) {
+	// 		return Task.getByUserId(parent.id)
+	// 	}
+	// },
+	// Task: {
+	// 	user(parent: Record<string, any>) {
+	// 		return User.getByTaskId(parent.id)
+	// 	}
+	// }
 }
 
 export default resolvers
